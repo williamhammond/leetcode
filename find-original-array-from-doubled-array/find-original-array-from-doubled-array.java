1a24937c3686543521b48936b31a730994d8120e
William Hammond <william.t.hammond@gmail.com>
2021-10-22 08:07:08 -0400

    Time: 596 ms (5.83%), Space: 127.5 MB (25.93%) - LeetHub

--- find-original-array-from-doubled-array/find-original-array-from-doubled-array.java
+++ find-original-array-from-doubled-array/find-original-array-from-doubled-array.java
@@ -6,42 +6,21 @@ class Solution {
             return new int[]{};   
         }
         SortedMap<Integer, Integer> values = new TreeMap<>();
-        for (int i: changed) {
-            if(values.containsKey(i)){
-                values.put(i, values.get(i) + 1);
-            } else {
-                values.put(i, 1);
-            }
+        for (int value: changed) {
+            values.put(value, values.getOrDefault(value, 0) + 1);
         }
-        
         int[] result = new int[changed.length / 2];
         int i = 0;
+        
         for (int value: values.keySet()) {
-            int count = values.get(value);
-            while (count > 0) {
-                if (value == 0)  {
-                    if (values.get(value) % 2 != 0) {
-                        return new int[]{};   
-                    }
-                    for (int j = 0; j < (values.get(value) / 2); j++) {
-                        result[i] = 0;
-                        i++;
-                    }
-                    count=0;
-                    values.put(0, 0);
-                } else {
-                    if (!values.containsKey(value * 2) || values.get(value * 2) < 1) {
-                        return new int[]{};   
-                    }
-
-                    result[i] = value;
-                    i++;
-
-                    values.put(value, values.get(value) - 1);
-                    values.put(value * 2, values.get(value * 2) - 1);
-                    count--;
-                }
+            if (values.get(value) > values.getOrDefault(value * 2, 0)) {
+                return new int[0];
             }
+            for (int j = 0; j < values.get(value); j++) {
+                result[i++] = value;
+                values.put(value * 2, values.get(value * 2) - 1);
+            }
+            
         }
         return result;
     }
