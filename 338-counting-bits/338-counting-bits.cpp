b02b0579e2bd95bd7e33c9b07edbc488d088c5d0
William Hammond <william.t.hammond@gmail.com>
2022-02-15 21:35:59 -0500

    Time: 35 ms (5.37%), Space: 13.9 MB (21.56%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -5,7 +5,7 @@ public:
     vector<int> countBits(int n) {
         vector<int> bits;
         for (int i = 0; i < n + 1; i++) {
-            std::string bitString = std::bitset<32>( i).to_string();
+            std::string bitString = std::bitset<17>( i).to_string();
             int oneCount = 0;
             for (int j = 0; j < bitString.length(); j++) {
                 if (bitString[j] == '1') oneCount++;
