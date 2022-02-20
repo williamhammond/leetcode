450f368c0dad3bc321c975cf4590b0db79ae848f
William Hammond <william.t.hammond@gmail.com>
2022-02-20 13:07:07 -0500

    Time: 36 ms (84.63%), Space: 13.9 MB (94.86%) - LeetHub

---
+++ 93-restore-ip-addresses/93-restore-ip-addresses.py
@@ -0,0 +1,25 @@
+import re
+
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        result = []
+        self.backtrack(s, 0, "", result)
+        return result
+    
+    # 0. Define base-case 
+    # 1. Express way of adding dot
+    # 2. Eject invalid result
+    def backtrack(self, s: str, index: int, current: str, result: List[str]):
+        if index == 4:
+            if not s:
+                result.append(current[:-1])
+            return
+        for i in range (1, 4):
+            if not s:
+                continue
+            octet = s[0: i]
+            if int(octet) > 255 or i != len(octet) or (len(octet) > 1 and octet[0] == "0"): 
+                continue
+            
+            self.backtrack(s[i:], index + 1, current + octet + ".", result);
+    
