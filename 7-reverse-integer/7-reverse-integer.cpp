8253f7124ce821fa4b6efbb54c3648d8309d1fe6
William Hammond <william.t.hammond@gmail.com>
2022-09-09 11:12:14 -0400

    Time: 0 ms (100.00%), Space: 5.9 MB (75.71%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cpp
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -6,8 +6,8 @@ public:
             if (y > INT_MAX / 10 || y < INT_MIN /10){
                 return 0;
             }
-            y=y * 10 + x % 10;
-            x= x / 10;
+            y = y * 10 + x % 10;
+            x = x / 10;
         }
         return y;
     }
