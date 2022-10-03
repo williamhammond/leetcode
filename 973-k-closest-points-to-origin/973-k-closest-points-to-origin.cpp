d7db0feba60e3ee9085ffc2bfc2f37d8dfb9a4cc
William Hammond <william.t.hammond@gmail.com>
2022-10-03 10:22:01 -0400

    Time: 1076 ms (10.60%), Space: 183.6 MB (6.52%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -5,6 +5,5 @@ public:
         auto comparator = [](vector<int> a, vector<int> b) { return a[0] * a[0] + a[1] * a[1] < b[0] * b[0] + b[1] * b[1];};
         partial_sort(points.begin(), points.begin() + k, points.end(), comparator);
         return vector<vector<int>>(points.begin(), points.begin() + k);
-
     }
 };
